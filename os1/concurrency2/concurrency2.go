366f088fc6269a691b506ac188792b2abe16a502
alesr <alessandro.resta@gmail.com>
2015-02-27 17:34:19 +0000

    update concurrency2

--- os1/concurrency2/concurrency2.go
+++ os1/concurrency2/concurrency2.go
@@ -5,43 +5,34 @@ import (
 )
 
 // send "x" to channel c
-func routineX(c chan string, done chan bool) {
+func routineX(c chan string) {
 	for {
-		select {
-		case c <- "x":
-		case <-done:
-			return
-		}
+		c <- "x"
 	}
 }
 
 // send "o" to channel c
-func routineO(c chan string, done chan bool) {
+func routineO(c chan string) {
 	for {
-		select {
-		case c <- "o":
-		case <-done:
-			return
-		}
+		c <- "o"
 	}
 }
 
 func main() {
 
 	strCh := make(chan string) // create a string channel
-	doneCh := make(chan bool)
 
-	go routineX(strCh, doneCh)
-	go routineO(strCh, doneCh)
+	go routineX(strCh)
+	go routineO(strCh)
 
 	// print time
 	for i := 1; i <= 10; i++ {
 		go fmt.Printf("%s", <-strCh)
 	}
-	for k := 1; k <= 10; k++ {
+	for j := 1; j <= 10; j++ {
 		go fmt.Printf("%s", <-strCh)
 	}
 
-	defer fmt.Printf("\n")
+	fmt.Printf("\n")
 
 }
