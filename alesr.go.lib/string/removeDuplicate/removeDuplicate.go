4da8e697ca1f7a82de8792011b51efa89e512b72
alesr <alessandro.resta@gmail.com>
2015-07-11 09:46:54 +0100

    adding removeDuplicate and _test to string lib

---
+++ alesr.go.lib/string/removeDuplicate/removeDuplicate.go
@@ -0,0 +1,20 @@
+package removeDuplicate
+
+// RemoveDuplicate items from array and set it to arr2
+func RemoveDuplicate(arr []string) []string {
+	arr2 := arr[:1]
+Loop:
+	for i := 1; i < len(arr); {
+		for j := 0; j < len(arr2); {
+			if arr[i] != arr[j] {
+				j++
+			} else {
+				i++
+				continue Loop
+			}
+		}
+		arr2 = append(arr2, arr[i])
+		i++
+	}
+	return arr2
+}
