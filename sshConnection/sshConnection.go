e25bb93e9726b787ee38cbfb088bf3b5a0d3e9ff
alesr <alessandro.resta@gmail.com>
2015-09-25 16:53:33 +0100

    ssh

--- sshConnection/sshConnection.go
+++ sshConnection/sshConnection.go
@@ -11,16 +11,31 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
+
+// DEBUG ALERT --> Uma instalação contemém métodos relacionados aos comandos.
+// e.g. adicionar comando, executar commando, ...
+// Yii é um tipo de Programa que contém commandos e implementa a interface
+// Installer.
+// Wp é outro tipo de Programa que contém commandos e implementa a interface
+// Installer.
+// Quaisquer novos sotfwares a serem instalados por este programas deverão ser
+// do tipo Programa e implementar a interface Installer.
 type ProjectField struct {
-	label , errorMsg, validationMsg string
+	name, label, inputQuestion, errorMsg, validationMsg string
+}
+
+type Program struct {
+	ProjectField
+	commands []string
 }
 
 type Project struct {
-	name      ProjectField
-	hostname  ProjectField
-	pwd       ProjectField
-	port      ProjectField
-	typ       ProjectField
+	projectname, hostname, pwd, port ProjectField
+	typ Program
+}
+
+type Installer interface {
+	connect()
 }
 
 func main() {
@@ -28,39 +43,136 @@ func main() {
 	// Initialization
 	project := new(Project)
 
+
+
 	project.assemblyLine()
-
-	project.name.errorMsg = "error getting the project's name: "
-	project.hostname.errorMsg = "error getting the project's hostname: "
-	project.pwd.errorMsg = "error getting the project's password: "
-	project.port.errorMsg = "error getting the project's port"
-	project.typ.errorMsg = "error getting the project's type"
-
-	project.name.validationMsg = "make sure you type a valid name for your project."
-	project.hostname.validationMsg = "make sure you type a valid hostname for your project."
-	project.pwd.validationMsg = "type a valid password. It must contain at least 6 digits"
-	project.port.validationMsg = "type a port between 1 and 999"
-	project.typ.validationMsg = "pay attention to the options"
-
 
 	// SSH connection config
 	config := &ssh.ClientConfig {
-		User: project.name.label,
+		User: project.projectname.name,
 		Auth: []ssh.AuthMethod{
-			ssh.Password(project.pwd.label),
+			ssh.Password(project.pwd.name),
 		},
 	}
 
-	command := "cd private && echo 'hello world' > hello.txt"
-	project.connect(command, config)
+	// commands := []string{
+	// 	"cd private && echo 'hello world' > hello.txt",
+	// 	}
+	// project.connect(commands, config)
 
 }
 
-func (p *Project) connect(command string, config *ssh.ClientConfig) {
+func (p *Project) assemblyLine() {
+
+	// Project name
+	p.projectname.inputQuestion = "project name: "
+	p.projectname.label = "projectname"
+	p.projectname.errorMsg = "error getting the project's name: "
+	p.projectname.validationMsg = "make sure you type a valid name for your project (3 to 20 characters)."
+	ask4Input(&p.projectname)
+
+	// Hostname
+	p.hostname.inputQuestion = "hostname: "
+	p.hostname.label = "hostname"
+	p.hostname.errorMsg = "error getting the project's hostname: "
+	p.hostname.validationMsg = "make sure you type a valid hostname for your project. it must contain '.com', '.pt' or '.org', for example.)."
+	ask4Input(&p.hostname)
+
+	// Password
+	p.pwd.inputQuestion = "password: "
+	p.pwd.label = "pwd"
+	p.pwd.errorMsg = "error getting the project's password: "
+	p.pwd.validationMsg = "type a valid password. It must contain at least 6 digits"
+	ask4Input(&p.pwd)
+
+	// Port
+	p.port.inputQuestion = "port (default 22): "
+	p.port.label = "port"
+	p.port.errorMsg = "error getting the project's port"
+	p.port.validationMsg = "only digits allowed. min 0, max 999."
+	ask4Input(&p.port)
+
+	// Type
+	p.typ.inputQuestion = "project type [1]yii [2]wp or gohugo: "
+	p.typ.label = "type"
+	p.typ.errorMsg = "error getting the project's type"
+	p.typ.validationMsg = "pay attention to the options"
+	ask4Input(&p.typ)
+}
+
+// Takes the assemblyLine's data and mount the prompt for the user.
+func ask4Input(field *ProjectField) {
+	consolereader := bufio.NewReader(os.Stdin)
+	fmt.Print(field.inputQuestion)
+	input, err := consolereader.ReadString('\n')
+	checkError(field.errorMsg, err)
+	checkInput(field, input)
+}
+
+
+// Right on hand error checker.
+func checkError(msg string, err error) {
+	if err != nil {
+		log.Fatal(msg, err.Error())
+	}
+}
+
+
+func checkInput(field *ProjectField, input string) {
+
+	var cleanInput string
+	var inputLength int
+
+	if len(input) > 1 {
+		cleanInput = strings.Fields(input)[0]
+		inputLength = len(cleanInput)
+	}
+
+	switch field.label {
+		case "projectname":
+
+			if inputLength < 3 || inputLength > 20 {
+				fmt.Println(field.validationMsg)
+				ask4Input(field)
+			}
+		case "hostname":
+			if inputLength <= 5 {
+				fmt.Println(field.validationMsg)
+				ask4Input(field)
+			}
+		case "pwd":
+			if inputLength <= 6 {
+				fmt.Println(field.validationMsg)
+				ask4Input(field)
+			}
+		case "port":
+
+			if inputLength == 0 {
+				cleanInput = "22"
+			} else if inputLength > 3 {
+				fmt.Println(field.validationMsg)
+				ask4Input(field)
+			}
+		case "type":
+			if cleanInput != "1" && cleanInput != "2" {
+				fmt.Println(field.validationMsg)
+				ask4Input(field)
+			} else if cleanInput == "1" {
+				cleanInput = "Yii"
+			} else if cleanInput == "2" {
+				cleanInput = "WP"
+			}
+	}
+
+	field.name = cleanInput
+}
+
+
+func (p *Project) connect(commands []string, config *ssh.ClientConfig) {
 
 	log.Printf("Trying connection...\n")
 
-	conn, err := ssh.Dial("tcp", fmt.Sprintf("%s:%s", p.hostname.label, p.port.label), config)
+	conn, err := ssh.Dial("tcp", fmt.Sprintf("%s:%s", p.hostname.name, p.port.name), config)
 	checkError("Failed to dial: ", err)
 	log.Printf("Connection established.\n")
 
@@ -74,106 +186,10 @@ func (p *Project) connect(command string, config *ssh.ClientConfig) {
 	var stdoutBuf bytes.Buffer
 	session.Stdout = &stdoutBuf
 
-	if err := session.Run(command); err != nil {
-		log.Fatal("Error on command execution", err.Error())
-	}
+	// log.Printf("Preparing to execute command: %s", commands[0])
+	// if err := session.Run(commands[0]); err != nil {
+	// 	log.Fatal("Error on command execution", err.Error())
+	// }
 
-}
-
-// Assembly questions and error messages for user's input
-func (p *Project) assemblyLine() {
-
-
-	// Project name
-	p.name.label = ask4Input("project name: ", p.name.errorMsg)
-	valid := checkInput(p.name.label, "name")
-	p.check4ValidInput(valid, "name")
-
-	// Hostname
-	p.hostname.label = ask4Input("hostname: ", p.hostname.errorMsg)
-	valid = checkInput(p.hostname.label, "hostname")
-	p.check4ValidInput(valid, "hostname")
-
-	// Password
-	p.pwd.label = ask4Input("Password: ", p.pwd.errorMsg)
-	valid = checkInput(p.hostname.label, "pwd")
-	p.check4ValidInput(valid, "pwd")
-
-	// Port
-	p.port.label = ask4Input("port (default 22): ", p.port.errorMsg)
-	valid = checkInput(p.hostname.label, "port")
-	p.check4ValidInput(valid, "port")
-
-	// Type
-	p.typ.label = ask4Input("project type [1]yii [2]wp or gohugo: ", p.typ.errorMsg)
-	valid = checkInput(p.hostname.label, "type")
-	p.check4ValidInput(valid, "type")
-}
-
-// Takes the assemblyLine's data and mount the prompt for the user.
-func ask4Input(question, errorMsg string) string {
-	consolereader := bufio.NewReader(os.Stdin)
-	fmt.Print(question)
-	s, err := consolereader.ReadString('\n')
-	checkError(errorMsg, err)
-
-	return removeDelimitator(s)
-}
-
-// Remove delimitator added by ReadString.
-func removeDelimitator(s string) string {
-	return strings.Fields(s)[0]
-}
-
-// Right on hand error checker.
-func checkError(msg string, err error) {
-	if err != nil {
-		log.Fatal(msg, err.Error())
-	}
-}
-
-
-// WAITING FOR REFACTORING
-func checkInput(input, kind string) bool {
-	switch kind {
-		case "name":
-			if len(input) <= 2 {
-				return false
-			}
-		case "hostname":
-			if len(input) <= 5 {
-				return false
-			}
-		case "pwd":
-			if len(input) <= 6 {
-				return false
-			}
-		case "port":
-			if len(input) == 0 {
-				return false
-			}
-		case "type":
-			if len(input) == 0 || len(input) > 1{
-				return false
-			}
-	}
-	return true
-
-}
-
-// Iterates over a string to check if it has digits.
-func hasDigits(input string) bool {
-	for _, value := range input {
-		if value >= '0' && value <= '9' {
-			return true
-		}
-	}
-	return false
-}
-// fmt.Sprintf("%s:%s", p.hostname.label, p.port.label)
-func (p *Project) check4ValidInput(flag bool, kind string)  {
-	if !flag {
-		fmt.Println(p.name.validationMsg)
-		p.name.label = ask4Input("project name: ", p.name.errorMsg)
-	}
+	log.Printf("Preparing to execute command: %s", p.typ.command[0])
 }
